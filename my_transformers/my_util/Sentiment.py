05b186c453ebdf7cc91ddd0ba167bbd1c62c1c78
rnfnal73 <rnfnal74@gmail.com>
2020-11-09 12:46:49 +0900

    new version

--- my_transformers/my_util/Sentiment.py
+++ my_transformers/my_util/Sentiment.py
@@ -1,10 +1,12 @@
 import pickle
 import torch
+import os
 
 def getSentiment():
+    file_path = os.path.dirname(os.path.realpath(__file__))
     weight = []
     senti_list = []
-    with open('/gdrive/My Drive/reheaven-PyTorch-master/my_transformers/my_util/senti_list.txt','rb') as fd:
+    with open(file_path+'/senti_list.txt','rb') as fd:
         senti_list = pickle.load(fd) # input_ids와 match되는 값
         weight = pickle.load(fd)
     return weight,senti_list
